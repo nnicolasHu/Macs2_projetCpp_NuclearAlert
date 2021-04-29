28819bb6783c4f8c54ba2276511dffb0ceeed552
matthieu <matthieu2209@live.com>
2021-04-29 22:20:51 +0200

    petit ajout sur la fonction niveau1

--- Graphic/Niveau1.cpp
+++ Graphic/Niveau1.cpp
@@ -4,5 +4,24 @@
 #include<SDL2/SDL_ttf.h>
 
 void Niveau1(){
-    
+    sdl2::font fonte_texte("./data/Lato-Thin.ttf", 18);
+    sdl2::font fonte_titre("./data/Lato-Bold.ttf", 28);
+    // Créer une fenêtre avec un titre et la résolution de la fenêtre (ici 800 x 600 pixels )
+    sdl2::window fenêtre("Nuclear Alert : Fenêtre de Controle", {800, 600});
+    bool quitting = false;
+
+    sdl2::texte niveau1("Niveau 1", fonte_titre, fenêtre, {0xFF, 0xFF, 0xFF});
+    niveau1.at(325, 50);
+    sdl2::texte help("Appuyer sur h pour afficher les commandes", fonte_texte, fenêtre, {0xFF, 0xFF, 0xFF, 0xFF});
+    help.at(10, 10);
+    sdl2::event_queue queue;
+
+    while(not quitting){
+        fenêtre << niveau1 << help << sdl2::flush;
+        auto events = queue.pull_events();
+        for(const auto& e : events){
+            if(e->kind_of_event() == sdl2::event::quit) quitting = true;
+        }
+
+    }  
 }
