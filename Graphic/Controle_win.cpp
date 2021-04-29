986c13255503b69ccd13fbeb1940539d20e9a4f0
matthieu <matthieu2209@live.com>
2021-04-29 09:36:17 +0200

    Modification de la fonction Controle_win

--- Graphic/Controle_win.cpp
+++ Graphic/Controle_win.cpp
@@ -12,10 +12,12 @@ void Controle_win(int level){
 
     sdl2::texte niveau1("Niveau 1", fonte_titre, fenêtre, {0xFF, 0xFF, 0xFF});
     niveau1.at(325, 50);
+    sdl2::texte help("Appuyer sur h pour afficher les commandes", fonte_texte, fenêtre, {0xFF, 0xFF, 0xFF, 0xFF});
+    help.at(10, 10);
     sdl2::event_queue queue;
     while(not quitting){
         if(level == 1){
-            fenêtre << niveau1 ;
+            fenêtre << niveau1 << help << sdl2::flush;
             auto events = queue.pull_events();
             for(const auto& e : events){
                 if(e->kind_of_event() == sdl2::event::quit) quitting = true;
