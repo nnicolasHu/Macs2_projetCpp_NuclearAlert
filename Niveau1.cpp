15c17077ac7b1a62259e5b9953b2a261e29356b1
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-05-05 01:18:50 +0200

    premier commit du niveau1

--- Niveau1.cpp
+++ Niveau1.cpp
@@ -4,12 +4,10 @@
 #include<SDL2/SDL_ttf.h>
 
 void Niveau1(sdl2::window* fenêtre){
+    bool quitting = false;
+    
     sdl2::font fonte_texte("./Graphic/data/Lato-Thin.ttf", 18);
     sdl2::font fonte_titre("./Graphic/data/Lato-Bold.ttf", 28);
-    // Créer une fenêtre avec un titre et la résolution de la fenêtre (ici 800 x 600 pixels )
-    //sdl2::window fenêtre("Nuclear Alert : Fenêtre de Controle", {800, 600});
-    //*fenêtre << sdl2::rectangle({0, 0}, {800, 600}, {0, 0, 0, 0});
-    bool quitting = false;
 
     sdl2::texte niveau1("Niveau 1", fonte_titre, *fenêtre, {0xFF, 0xFF, 0xFF});
     niveau1.at(325, 50);
@@ -17,27 +15,18 @@ void Niveau1(sdl2::window* fenêtre){
     help.at(10, 10);
     sdl2::event_queue queue;
 
-    while(not quitting){
+    while(not(quitting)){
         *fenêtre << niveau1 << help << sdl2::flush;
+        std::cout << "je suis la" << std::endl;
         auto events = queue.pull_events();
         for(const auto& e : events){
             if(e->kind_of_event() == sdl2::event::quit) quitting = true;
             if(e->kind_of_event() == sdl2::event::key){
                 auto& key = dynamic_cast<sdl2::event_keyboard&>(*e);
-                if(key.state() == sdl2::event_keyboard::pressed){
-                    char keychar = key.ascci_code();
-                    if(keychar == 27) quitting = true;
-                    else if(keychar == 'h') {
-                        fenêtre->cls({0, 0, 0, 0});
-                        Commande();
-                    }
-                    else if(keychar == 9) {
-                        fenêtre->cls({0, 0, 0, 0});
-                        Centrale_tab(fenêtre);
-                    }
-                }
+                char keychar = key.ascci_code();
+               
             }
         }
+    }
 
-    }  
 }
