2a47b9e11b9a9672c111b2501df5e720d8e183b8
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-05-07 00:38:37 +0200

    ajout fonction arrondi

--- header/Graphic_Fonction.hpp
+++ header/Graphic_Fonction.hpp
@@ -1,13 +1,17 @@
 #ifndef _GRAPHIC_FONCTION_HPP_
 #define _GRAPHIC_FONCTION_HPP_
+
 #include<iostream>
-#include"centrale.hpp"
 #include<string>
+#include <cmath>
+
+#include"centrale.hpp"
 #include"sdl2.hpp"
+
 
 using namespace std::string_literals ;// Indispensable pour utiliser le suffixe s
 
-
+double arrondi(double x, int i);
 std::string Fenetre_acceuil(sdl2::window* fenêtre);
 int Choix_Niveau(sdl2::window* fenêtre);
 void Centrale_tab(sdl2::window* fenêtre);
@@ -15,5 +19,6 @@ void Commande(sdl2::window * fenêtre);
 void Niveau1(sdl2::window* fenêtre);
 void SalleDeControle(sdl2::window* fenêtre, centrale* C);
 void Radioprotection_win(sdl2::window* fenêtre, centrale* C);
+
 #endif
 
