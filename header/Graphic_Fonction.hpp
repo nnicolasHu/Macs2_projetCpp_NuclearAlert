55e611ca1ae3ded2eafa8c13c91fccb874285368
chrissive <christophesive@MacBook-Pro-de-Christophe.local>
2021-05-11 22:11:41 +0200

    maj

--- header/Graphic_Fonction.hpp
+++ header/Graphic_Fonction.hpp
@@ -15,10 +15,12 @@ std::string arrondi(double x, int i);
 std::string Fenetre_acceuil(sdl2::window* fenêtre);
 int Choix_Niveau(sdl2::window* fenêtre);
 void Centrale_tab(sdl2::window* fenêtre);
-void Commande(sdl2::window * fenêtre);
+void Commande1(sdl2::window * fenêtre);
+void Commande2(sdl2::window * fenêtre);
 void Niveau1(sdl2::window* fenêtre);
 void SalleDeControle(sdl2::window* fenêtre, centrale* C);
 void Radioprotection_win(sdl2::window* fenêtre, centrale* C);
+void InterventionO(sdl2::window * fenêtre);
 
 #endif
 
