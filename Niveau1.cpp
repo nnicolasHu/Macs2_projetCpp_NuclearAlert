a1c82dafba92159c4ab19de075f4c3bf29c7169d
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-05-13 06:15:53 +0200

    ajout header pour niveau1

--- Niveau1.cpp
+++ Niveau1.cpp
@@ -1,29 +1,27 @@
 #include<iostream>
-#include"Graphic_Fonction.hpp"
-#include"sdl2.hpp"
-#include<SDL2/SDL_ttf.h>
+#include"niveaux.hpp"
 
-void Niveau1(sdl2::window* fenêtre){
+void Niveau1(sdl2::window& fenêtre){
     bool quitting = false;
     
     sdl2::font fonte_texte("./Graphic/data/Lato-Thin.ttf", 18);
     sdl2::font fonte_titre("./Graphic/data/Lato-Bold.ttf", 28);
 
-    sdl2::texte niveau1("Niveau 1", fonte_titre, *fenêtre, {0xFF, 0xFF, 0xFF});
+    sdl2::texte niveau1("Niveau 1", fonte_titre, fenêtre, {0xFF, 0xFF, 0xFF});
     niveau1.at(325, 50);
-    sdl2::texte help("Appuyer sur h pour afficher les commandes", fonte_texte, *fenêtre, {0xFF, 0xFF, 0xFF, 0xFF});
+    sdl2::texte help("Appuyer sur h pour afficher les commandes", fonte_texte, fenêtre, {0xFF, 0xFF, 0xFF, 0xFF});
     help.at(10, 10);
     sdl2::event_queue queue;
 
     while(not(quitting)){
-        *fenêtre << niveau1 << help << sdl2::flush;
+        fenêtre << niveau1 << help << sdl2::flush;
         std::cout << "je suis la" << std::endl;
         auto events = queue.pull_events();
         for(const auto& e : events){
             if(e->kind_of_event() == sdl2::event::quit) quitting = true;
             if(e->kind_of_event() == sdl2::event::key){
                 auto& key = dynamic_cast<sdl2::event_keyboard&>(*e);
-                char keychar = key.ascci_code();
+                //char keychar = key.ascci_code();
                
             }
         }
---
+++ header/niveaux.hpp
@@ -0,0 +1,7 @@
+#ifndef _NIVEAUX_HPP_
+#define _NIVEAUX_HPP_
+
+#include "Graphic_Fonction.hpp"
+
+void Niveau1(sdl2::window& fenêtre);
+#endif
